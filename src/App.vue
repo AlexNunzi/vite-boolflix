1b4c6f9aa6413b4c93a95de51cc60570694d0f6e
Alex Nunzi <alex.nunzi@hotmail.it>
2023-03-31 15:04:00 +0200

    first commit

---
+++ src/App.vue
@@ -0,0 +1,24 @@
+<template>
+  <TheHeader />
+</template>
+
+<script>
+import axios from 'axios';
+import { storage } from './storage.js';
+
+import TheHeader from './components/TheHeader.vue';
+export default{
+  data() {
+    return {
+      storage
+    }
+  },
+  components: {
+    TheHeader,
+  }
+}
+</script>
+
+<style lang="scss">
+@use "./styles/general.scss";
+</style>
---
+++ src/components/TheHeader.vue
@@ -0,0 +1,30 @@
+<template>
+    <header>
+        <div class="container d-flex justify-content-between align-center h-100 m-auto">
+            <h1>BOOLFLIX</h1>
+            <div>
+                <input class="me-3" type="text">
+                <button>Cerca</button>
+            </div>
+        </div>
+    </header>
+</template>
+
+<script>
+export default{
+    name: 'TheHeader',
+}
+</script>
+
+<style lang="scss">
+@use '../styles/variables' as *;
+
+header { 
+    background-color: $bg-header;
+    height: 5rem;
+
+    & h1 {
+        color: $text-color-header;
+    }
+}
+</style>
