0164a54169fdf8d821502772bf44c52657a3df11
Alex Nunzi <alex.nunzi@hotmail.it>
2023-03-31 16:23:27 +0200

    milestone 1: Corretto tipo di dato ricevuto tramite props dal component CardFilm

--- src/components/CardFilm.vue
+++ src/components/CardFilm.vue
@@ -18,7 +18,7 @@ export default{
         originalFilmName: String,
         translatedFilmName: String,
         languageFilm: String,
-        voteAvarage: String,
+        voteAvarage: Number,
 
     }
 }
