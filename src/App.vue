43b23bd57c263daa7b38ab5d2db6472cdba5ce15
Alex Nunzi <alex.nunzi@hotmail.it>
2023-04-03 12:09:57 +0200

    Refactoring: eliminati components FilmList, SeriesList, CardFilm, CardSerie e utilizzato un componente più flessibile per la visualizzazione delle card sia per i film che per le serie tv

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,33 @@
 <template>
   <TheHeader @searchFilm="search"/>
-  <main class="my-3">
+  <main class="my-3 container m-auto">
     <h2 v-if="foundSomething()" class="text-center">{{ this.storage.uiMessage }}</h2>
+
+<!-- FILM/TV SHOW SECTION -->
     <div v-else>
-      <FilmList />
-      <SeriesList />
+
+    <!-- FILM -->
+      <section>
+        <h2 class="my-3">Lista dei film:</h2>
+        <ul v-if="storage.filmList.length > 0" class="list-style-none d-flex flex-wrap m-auto">
+          <CardFilmSerie class="card-container" v-for="film in storage.filmList"
+          :filmOrTv="film"
+          />
+        </ul>
+        <h3 v-else>Non sono stati trovati film che contengano "{{ storage.searchInput }}" nel proprio titolo</h3>
+      </section>
+        
+    <!-- TV SHOW -->
+      <section>
+        <h2 class="my-3">Lista delle serie tv:</h2>
+        <ul v-if="storage.seriesList.length > 0" class="list-style-none d-flex flex-wrap m-auto">
+            <CardFilmSerie class="card-container" v-for="serie in storage.seriesList"
+              :filmOrTv="serie"
+            />
+        </ul>
+        <h3 v-else>Non sono state trovate serie TV che contengano "{{ storage.searchInput }}" nel proprio titolo</h3>
+      </section>
+
     </div>
   </main>
 </template>
@@ -14,8 +37,7 @@ import axios from 'axios';
 import { storage } from './storage.js';
 
 import TheHeader from './components/TheHeader.vue';
-import FilmList from './components/FilmList.vue';
-import SeriesList from './components/SeriesList.vue';
+import CardFilmSerie from './components/CardFilmSerie.vue';
 export default{
   data() {
     return {
@@ -24,8 +46,7 @@ export default{
   },
   components: {
     TheHeader,
-    FilmList,
-    SeriesList
+    CardFilmSerie
   },
   methods: {
     urlApi(type, ...parameters){
---
+++ src/components/CardFilmSerie.vue
@@ -0,0 +1,44 @@
+<template>
+    <div class="card p-3">
+            <li>
+                <img class="w-100" :src="`https://image.tmdb.org/t/p/w342/${filmOrTv.poster_path}`" alt="Film cover image">
+                <h4 class="my-3">Il titolo tradotto è: {{ filmOrTv.title ? filmOrTv.title : filmOrTv.name }}</h4>
+                <h3 class="my-3">Il titolo originale è: {{ filmOrTv.original_title ? filmOrTv.original_title : filmOrTv.name }}</h3>
+                <h5 class="my-3">Lingua originale: 
+                    <img class="flag" v-if="storage.flagImg[filmOrTv.original_language]" :src="storage.flagImg[filmOrTv.original_language]">
+                    <span v-else>{{ filmOrTv.original_language }}</span>
+                </h5>
+                <h5 class="my-3">Il voto medio ricevuto è: {{ filmOrTv.vote_average / 2 }}</h5>
+                <RatingStars :voteStars="filmOrTv.vote_average / 2" />
+            </li>
+    </div>
+</template>
+
+<script>
+import { storage } from '../storage';
+import RatingStars from './RatingStars.vue';
+export default{
+    name: 'CardFilm',
+    data() {
+        return {
+            storage,
+        }
+    },
+    props: {
+        filmOrTv: Object,
+    },
+    components:{
+        RatingStars,
+    }
+}
+</script>
+
+<style scoped lang="scss">
+.card{
+    border: 2px solid gray;
+}
+
+.flag{
+    width: 2rem;
+}
+</style>
